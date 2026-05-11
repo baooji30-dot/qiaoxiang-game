fbb69a5999ca5ee562fc5bd175dcdddde007c722
Baooji30-dot <baooji30@gmail.com>
2026-05-11 14:40:05 +0800

    fix render start

--- server/index.js
+++ server/index.js
@@ -1,28 +1,17 @@
-const express = require("express");
 const path = require("path");
 
-const app = express();
+app.use(express.static(
+  path.join(process.cwd(), "client/dist")
+));
 
-// 静态文件
-app.use(
-  express.static(
-    path.join(process.cwd(), "client/dist")
-  )
-);
-
-// React/Vite 路由
 app.get("*", (req, res) => {
   res.sendFile(
-    path.join(
-      process.cwd(),
-      "client/dist/index.html"
-    )
+    path.join(process.cwd(), "client/dist/index.html")
   );
 });
 
-// 启动服务器
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log("服务器启动:", PORT);
+http.listen(PORT, () => {
+  console.log("服务器启动", PORT);
 });
